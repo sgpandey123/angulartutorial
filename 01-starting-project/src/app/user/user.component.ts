91090a8463ecf4e4a642c7824dad5d42f6dfb763
Sanjay Pandey <sgpandey123@gmail.com>
2025-04-17 21:34:07 +0530

    completed tutorial 46

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,5 +1,11 @@
 import { Component , Input, EventEmitter, Output} from '@angular/core';
 
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -7,21 +13,18 @@ import { Component , Input, EventEmitter, Output} from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
-  @Input({required : true}) id!: string;
-  @Input({required : true}) avatar!: string;
-  @Input({required : true}) name!: string;
 
+export class UserComponent {
+  @Input({required : true}) user!: User;
   @Output() select = new EventEmitter<string>();
   
-
   get imagePath(){
-    return 'assets/users/' + this.avatar ; 
-  }
-  onSelectUser(){
-    this.select.emit(this.id);
+    return 'assets/users/' + this.user.avatar ; 
   }
 
+  onSelectUser(){
+    this.select.emit(this.user.id);
+  }
 
 }
 
